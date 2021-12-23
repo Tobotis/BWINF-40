802986ce11d9f1ebd0fce020945380ce5c0ee821
Tobotis <to.steinbrecher@gmail.com>
2021-12-23 14:57:27 +0100

    first solution

--- Aufgabe3-Implementierung/A3.py
+++ Aufgabe3-Implementierung/A3.py
@@ -5,62 +5,103 @@ from os.path import exists
 # Zeitmessung der Execution-Time
 import time
 
-
+# Dictinoary mit den Segmenten des Sieben-Segment-Displays
+# Zum Konvertieren einer Hexadezimalzahl (Key) in eine Liste mit den Segmenten, die "leuchten" (Value)
+# 1: Segment ist an; 2: Segment ist aus
+# Indizes starten beim obersten Segment (0) und folgen dem Urzeigersinn => Index 6 ist das mittlere Segement
 hexInSSD = {
-    "0": [1, 1, 1, 1, 1, 1, 0],
-    "1": [0, 1, 1, 0, 0, 0, 0],
-    "2": [1, 1, 0, 1, 1, 0, 1],
-    "3": [1, 1, 1, 1, 0, 0, 1],
-    "4": [0, 1, 1, 0, 0, 1, 1],
-    "5": [1, 0, 1, 1, 0, 1, 1],
-    "6": [1, 0, 1, 1, 1, 1, 1],
-    "7": [1, 1, 1, 0, 0, 0, 0],
-    "8": [1, 1, 1, 1, 1, 1, 1],
-    "9": [1, 1, 1, 1, 0, 1, 1],
-    "A": [1, 1, 1, 0, 1, 1, 1],
-    "B": [0, 0, 1, 1, 1, 1, 1],
-    "C": [1, 0, 0, 1, 1, 1, 0],
-    "D": [0, 1, 1, 1, 1, 0, 1],
-    "E": [1, 0, 0, 1, 1, 1, 1],
     "F": [1, 0, 0, 0, 1, 1, 1],
+    "E": [1, 0, 0, 1, 1, 1, 1],
+    "D": [0, 1, 1, 1, 1, 0, 1],
+    "C": [1, 0, 0, 1, 1, 1, 0],
+    "B": [0, 0, 1, 1, 1, 1, 1],
+    "A": [1, 1, 1, 0, 1, 1, 1],
+    "9": [1, 1, 1, 1, 0, 1, 1],
+    "8": [1, 1, 1, 1, 1, 1, 1],
+    "7": [1, 1, 1, 0, 0, 0, 0],
+    "6": [1, 0, 1, 1, 1, 1, 1],
+    "5": [1, 0, 1, 1, 0, 1, 1],
+    "4": [0, 1, 1, 0, 0, 1, 1],
+    "3": [1, 1, 1, 1, 0, 0, 1],
+    "2": [1, 1, 0, 1, 1, 0, 1],
+    "1": [0, 1, 1, 0, 0, 0, 0],
+    "0": [1, 1, 1, 1, 1, 1, 0],
 }
-def umwandlungen(übrigerUmsatz, übrigeUmlegungen, ziffer):
-    # Umformungen der Ziffern in eine andere Ziffer
-    # [Anzahl der Umlegungen, Umsatz (wie viele Stäbchen bleiben insgesamt über/werde insegesamt abgegeben)]
-    umwandlungen = {
-        ziffer: (0, 0),
-    }
+
+# Die Umwandlung einer Hexadezimalzahl im Sieben-Segment-Display kann ausgeführt werden
+# übrigerUmsatz = Segemnte, die nach dem umwandeln übrig sind
+# übrigeUmelgungen = Umlegungen, die maximal getätigt werden dürfen
+# index = Index der aktuellen Ziffer in der Hexadezimalzahl
+# hexZahl = Hexadezimalzahl, die umwandelt werden soll
+# nurErhöhung = True, wenn die eingebene Ziffer nur erhöht werden darf
+# schritte = Liste der Schritte/Umlegungen, die getätigt wurden
+
+
+def umwandeln(übrigerUmsatz, übrigeUmlegungen, index, hexZahl, nurErhöhung=False, schritte=[]):
+    # Check ob alle Ziffern umwandelt wurden => man ist am Ende der Hexzahl angekommen
+    if index >= len(hexZahl):
+        # Check ob Segmente übrig sind => Die Lösung ist nich valid
+        if übrigerUmsatz != 0:
+            return []
+        # Die Lösung ist valid und die Schritte können zurückgegeben werden
+        return [schritte, übrigeUmlegungen]
+    # Festlegen der aktuellen Ziffer
+    ziffer = hexZahl[index]
+    # Iteration über alle anderen Hexziffern von F bis 0
     for i in hexInSSD.keys():
-        if i != ziffer:
-            übrigeSegemente = übrigerUmsatz
-            anzahlUmlegungen = 0
-            for segment in range(7):
-                # Check ob das Segment in der Ausgangsziffer fehlt
-                if hexInSSD[i][segment] > hexInSSD[ziffer][segment]:
-                    # Es wird nur eine neue Umlegung durchgeführt, wenn es keine übrigen Segmente gibt
-                    anzahlUmlegungen += 1 if übrigeSegemente <= 0 else 0
-                    # Ein "übrigesSegment" wird verwendet (selbst wenn keine übrig sind)
-                    übrigeSegemente -= 1
-                # Check ob ein Segment in der Ausgangsziffer zu viel ist
-                elif hexInSSD[i][segment] < hexInSSD[ziffer][segment]:
-                    # Es wird nur eine neue Umlegung durchgeführt, wenn es woanders kein "Bedarf" gibt
-                    anzahlUmlegungen += 0 if übrigeSegemente < 0 else 1
-                    # Ein "übrigesSegment" wird hinzugefügt
-                    übrigeSegemente += 1
-                # Die Umformung ist nicht möglich, wenn die übrige Umlegungen nicht genug sind
-                if anzahlUmlegungen > übrigeUmlegungen:
-                    break
+        # Check ob man bei der aktuellen Ziffen angekommen ist
+        # Es folgen somit niedrigere Hexziffern
+        if i == ziffer and nurErhöhung:
+            # Falls nur höhere Hexziffern überprüft werden sollten, wird die Schleife verlassen
+            break
+        # Die übrigenSegmente entsprechen dem übrigen Umsatz
+        übrigeSegemente = übrigerUmsatz
+        # Anzahl der Umlegungen nach i wird mit 0 initialisiert
+        anzahlUmlegungen = 0
+        # Iteration über alle Segmente der Ziffern
+        for segment in range(7):
+            # Check ob das Segment von i in der Ausgangsziffer fehlt
+            if hexInSSD[i][segment] > hexInSSD[ziffer][segment]:
+                # Es wird nur eine neue Umlegung durchgeführt, wenn es keine übrigen Segmente gibt
+                anzahlUmlegungen += 1 if übrigeSegemente <= 0 else 0
+                # Ein "übrigesSegment" wird verwendet (selbst wenn keine übrig sind =>
+                # möglicherweise kann es in der nächsten Ziffer erzeugt werden)
+                übrigeSegemente -= 1
+            # Check ob ein Segment von i in der Ausgangsziffer zu viel ist
+            elif hexInSSD[i][segment] < hexInSSD[ziffer][segment]:
+                # Es wird nur eine neue Umlegung durchgeführt, wenn es woanders kein "Bedarf" gibt
+                anzahlUmlegungen += 0 if übrigeSegemente < 0 else 1
+                # Ein "übrigesSegment" wird hinzugefügt
+                übrigeSegemente += 1
+            # Die Umformung ist nicht möglich, wenn die übrige Umlegungen nicht genug sind
+            if anzahlUmlegungen > übrigeUmlegungen:
+                break
+        else:
+            # Wenn nicht aus der for-Loop gebreakt wurde, kann die Ziffer offensichtlich un Ziffer i umwandelt werden
+            # Darauf basierend, werden die Schritte für die nächste Ziffer erzeugt
+            result = umwandeln(übrigerUmsatz+übrigeSegemente, übrigeUmlegungen -
+                               anzahlUmlegungen, index+1, hexZahl, False, schritte+[[ziffer, i]])
+            # Wenn eine Lösung gefunden wurde, wird diese zurückgegeben
+            # Es ist die größtmögliche, da von F nach 0 iteriert wird
+            if len(result) > 0:
+                return result
+    # Es wurde keine Lösung gefunden
+    return []
+
+
+def solve(hexZahl, maxUmlegungen):
+    for index in range(len(hexZahl)):
+        result = umwandeln(0, maxUmlegungen, index,
+                           hexZahl, nurErhöhung=True)
+        if len(result) > 0 or index == len(hexZahl)-1:
+            if result == []:
+                print("No solution found")
             else:
-                # Wenn nicht aus der for-Loop gebreakt wurde
-                umwandlungen[i] = (anzahlUmlegungen, übrigeSegemente)
-
-    return umwandlungen
-
-
-
-def solve(hexZahl):
-    for ziffer in hexZahl:
-        umwandlungen(0,3,ziffer)
+                ergebnis = ""
+                for ziffer in result[0]:
+                    ergebnis += ziffer[1]
+                print(ergebnis, "mit", maxUmlegungen-result[1], "Umlegungen")
+                return
 
 
 def parseInput():
@@ -75,7 +116,8 @@ def parseInput():
         inhalt = data.readlines()  # Lesen der Zeilen
         inhalt = [i.replace("\n", "")
                   for i in inhalt[1:]]  # Zeilenumbrüche entfernen
-        return inhalt
+        data = [i.split(" ") for i in inhalt]
+        return data
 
 
 def main():  # Startpunkt des Programmes
@@ -84,7 +126,8 @@ def main():  # Startpunkt des Programmes
         return
     # Lösen des Problems
     for i in input:
-        solve(i)
+        print("SOLVING:", i)
+        solve(i[0], int(i[1]))
 
 
 if __name__ == "__main__":  # Das ist Python :)
